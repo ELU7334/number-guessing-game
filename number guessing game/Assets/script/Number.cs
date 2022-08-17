13b8252fee3bfc854ab34818d16f79e7474ad224
ELU7334 <111079531+ELU7334@users.noreply.github.com>
2022-08-17 11:03:34 -0700

    update

---
+++ number guessing game/Assets/script/Number.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Number : MonoBehaviour
+{ 
+    int guess;
+    int minValue = 1;
+    int maxValue = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to our Number Guessing Game.");
+        Debug.Log("The number range is from " + minValue +" and" + maxValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
